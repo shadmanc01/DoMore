d75341af5e82471b764ce5797f5f7fc0722b4ac4
Shadman <shadmanchowdhury01@gmail.com>
2022-09-19 15:21:40 -0400

    weather bug fixes

--- weather.js
+++ weather.js
@@ -4,8 +4,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			console.log('My General Position:', position);
 			const lon = position.coords.longitude;
 			const lat = position.coords.latitude;
-            console.log(lat);
-            console.log(lon)
 
 			fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e6e8be293abf4cc4e1ebcc1e9730d1a7`)
 			.then(resp => resp.json())
