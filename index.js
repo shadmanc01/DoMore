41c40431d439cf2ed218d591a16795c0deccab9b
Ashe <ashe@Ashes-MacBook-Pro.local>
2022-09-20 08:32:36 -0400

    pushing to be able to pull

--- index.js
+++ index.js
@@ -64,45 +64,60 @@ function closeModal(modal) {
 }
 
 // clock
-    function updateClock(){
-      let now = new Date();
-      let dname = now.getDay(),
-          mo = now.getMonth(),
-          dnum = now.getDate(),
-          yr = now.getFullYear(),
-          hou = now.getHours(),
-          min = now.getMinutes(),
-          sec = now.getSeconds(),
-          pe = "AM";
-
-          if(hou >= 12){
-            pe = "PM";
-          }
-          if(hou == 0){
-            hou = 12;
-          }
-          if(hou > 12){
-            hou = hou - 12;
-          }
-
-          Number.prototype.pad = function(digits){
-            for(let n = this.toString(); n.length < digits; n = 0 + n);
-            return n;
-          }
-
-          let months = ["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
-          let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-          let ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
-          let values = [week[dname], months[mo], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
-          for(let i = 0; i < ids.length; i++)
-          document.getElementById(ids[i]).firstChild.nodeValue = values[i];
-    }
-
-    function initClock(){
-      updateClock();
-      window.setInterval("updateClock()", 1);
-    }
+window.addEventListener("load", () => {
+    clock();
+    function clock() {
+      const today = new Date();
+  
+      
+      const hours = today.getHours();
+      const minutes = today.getMinutes();
+      const seconds = today.getSeconds();
+  
+     
+      const hour = hours < 10 ? "0" + hours : hours;
+      const minute = minutes < 10 ? "0" + minutes : minutes;
+      const second = seconds < 10 ? "0" + seconds : seconds;
+  
+      
+      const hourTime = hour > 12 ? hour - 12 : hour;
+  
     
+      const ampm = hour < 12 ? "AM" : "PM";
+  
+      
+      const month = today.getMonth();
+      const year = today.getFullYear();
+      const day = today.getDate();
+  
+      
+      const monthList = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+  
+      
+      const date = monthList[month] + " " + day + ", " + year;
+      const time = hourTime + ":" + minute + ":" + second + ampm;
+  
+      
+      const dateTime = date + " - " + time;
+  
+      
+      document.getElementById("date-time").innerHTML = dateTime;
+      setTimeout(clock, 1000);
+    }
+  });
     // clock end
     window.addEventListener("load", function(){
         setTimeout(
