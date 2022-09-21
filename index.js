c4e5fc4c86879b6956ab1950d118e0e915eac578
Ashe <ashe@Ashes-MacBook-Pro.local>
2022-09-21 18:17:51 -0400

    world clock added

--- index.js
+++ index.js
@@ -28,62 +28,6 @@ const placeHolder2 = "Still Feeling Down?"
 //
 
 
-// clock
-window.addEventListener("load", () => {
-  clock();
-  function clock() {
-    const today = new Date();
-
-
-    const hours = today.getHours();
-    const minutes = today.getMinutes();
-    const seconds = today.getSeconds();
-
-
-    const hour = hours < 10 ? "0" + hours : hours;
-    const minute = minutes < 10 ? "0" + minutes : minutes;
-    const second = seconds < 10 ? "0" + seconds : seconds;
-
-
-    const hourTime = hour > 12 ? hour - 12 : hour;
-
-
-    const ampm = hour < 12 ? "AM" : "PM";
-
-
-    const month = today.getMonth();
-    const year = today.getFullYear();
-    const day = today.getDate();
-
-
-    const monthList = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
-
-
-    const date = monthList[month] + " " + day + ", " + year;
-    const time = hourTime + ":" + minute + ":" + second + ampm;
-
-
-    const dateTime = date + " - " + time;
-
-
-    document.getElementById("date-time").innerHTML = dateTime;
-    setTimeout(clock, 1000);
-  }
-});
-// clock end
 
 
 // Brain Dump ------------------->
@@ -125,9 +69,6 @@ function closeModal(modal) {
   overlay.classList.remove('active')
 }
 
-
-    
-    // clock end
     window.addEventListener("load", function(){
         setTimeout(
             function open(event){
